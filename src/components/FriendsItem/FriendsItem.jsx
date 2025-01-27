1b85ef3d3b91b54d810e355aadba2e6bd30ee842
PdaDmitry <pda.gazcomp@gmail.com>
2025-01-27 15:20:31 +0200

    Update FriendsItem.jsx  (address)

--- src/components/FriendsItem/FriendsItem.jsx
+++ src/components/FriendsItem/FriendsItem.jsx
@@ -1,6 +1,6 @@
 import { useSelector } from 'react-redux';
 import css from './FriendsItem.module.css';
-import { selectFriendById, selectFriends } from '../../redux/friends/selectorFriends';
+import { selectFriendById } from '../../redux/friends/selectorFriends';
 
 export const FriendsItem = ({ id }) => {
   const friend = useSelector(selectFriendById(id));
@@ -9,23 +9,68 @@ export const FriendsItem = ({ id }) => {
     return <p>Friend not found</p>;
   }
 
-  const { address, email, phone, title, imageUrl } = friend;
+  const { address, email, phone, title, imageUrl, addressUrl } = friend;
+
+  // let workDays;
+  // let from;
+  // let to;
+  // if (friend.workDays == null) workDays = 'Day and night';
+  // if (Array.isArray(friend.workDays)) {
+  //   for (let i = 0; i < friend.workDays.length; i++) {
+  //     if (friend.workDays[i].from === '') {
+  //       continue;
+  //     } else {
+  //       from = friend.workDays[i].from;
+  //     }
+  //     if (friend.workDays[i].to === '') {
+  //       continue;
+  //     } else {
+  //       to = friend.workDays[i].to;
+  //     }
+  //   }
+  // }
+
+  // let from, to;
+  let workDaysText = 'Day and night'; // Значение по умолчанию
+
+  // if (Array.isArray(friend.workDays) && friend.workDays.length > 0) {
+  //   from = friend.workDays.find(day => day.from)?.from || '';
+
+  //   to = friend.workDays.reverse().find(day => day.to)?.to || '';
+
+  //   if (from && to) {
+  //     workDaysText = `${from} - ${to}`;
+  //   }
+  // }
 
   return (
     <div className={css.contFriend}>
-      <div className={css.workDays}></div>
+      <div className={css.workDays}>{workDaysText}</div>
       <img src={imageUrl} alt={`${title}'s photo`} className={css.logoFriend} />
 
       <div>
         <h2 className={css.title}>{title}</h2>
-        <p>Email: {email}</p>
-        <p>Address: {address}</p>
-        <p>Phone: {phone}</p>
-        {/* <p>Work Days: {workDays}</p> */}
-        {/* <img src={imageUrl} alt={`${title}'s photo`} /> */}
-        {/* <a href={addressUrl} target="_blank" rel="noopener noreferrer">
-        Visit profile
-      </a> */}
+
+        <address>
+          <ul className={css.menuAddress}>
+            <li>
+              <a href={`mailto:${email}`}>Email: {email ? ` ${email}` : 'Website only'}</a>
+            </li>
+            <li>
+              <a href={addressUrl} target="_blank" rel="noopener noreferrer">
+                Address:{' '}
+                {address
+                  ? address.length > 19
+                    ? `${address.slice(0, 19)}...`
+                    : address
+                  : 'Website only'}
+              </a>
+            </li>
+            <li>
+              <a href={`tel:${phone}`}>Phone: {phone ? ` ${phone}` : 'Email only'}</a>
+            </li>
+          </ul>
+        </address>
       </div>
     </div>
   );
