1ca8f5e72a8b76ba01201adc29663a5a2dbe5366
PdaDmitry <146446934+PdaDmitry@users.noreply.github.com>
2025-02-15 20:34:07 +0200

    Create NotFound page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,3 +1,32 @@
+import { useNavigate } from 'react-router-dom';
+import css from './NotFound.module.css';
+
 export const NotFound = () => {
-  return <h1>NotFound</h1>;
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/home');
+  };
+
+  return (
+    <div className={css.conteiner}>
+      <div className={css.contNotFound}>
+        <div className={css.title}>
+          <p className={css.numberFor}>4</p>
+          <span className={css.notFoundSpan}>
+            <img
+              src="../../../public/images/not-found/not-found-mob.png"
+              alt="Not Found"
+              className={css.image}
+            />
+          </span>
+          <p className={css.numberFor}>4</p>
+        </div>
+        <p className={css.text}>Ooops! This page not found :(</p>
+        <button type="button" className={css.btnGoHome} onClick={handleClick}>
+          To home page
+        </button>
+      </div>
+    </div>
+  );
 };
--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,5 +1,37 @@
-// import css from './ProfilePage.module.css';
+import Title from '../../components/Title/Title';
+import css from './ProfilePage.module.css';
 
 export const ProfilePage = () => {
-  return <h1>ProfilePage</h1>;
+  return (
+    <div className={css.conteiner}>
+      <div className={css.contProfile}>
+        <div className={css.contUserUdateBtns}>
+          <button type="button" className={css.btnUser}>
+            <span className={css.spanBtnUser}>User</span>
+            <svg className={css.userSvg}>
+              <use href="/symbol-defs-mob.svg#icon-user-02-1"></use>
+            </svg>
+          </button>
+          <button type="button" className={css.btnUpdateUser}>
+            <svg className={css.editSvg}>
+              <use href="/symbol-defs-mob.svg#icon-edit-2"></use>
+            </svg>
+          </button>
+        </div>
+        {/* =============================================================== */}
+        <div className={css.contPhoto}>
+          <div className={css.photo}>
+            <svg className={css.userSvgPhoto}>
+              <use href="/symbol-defs-mob.svg#icon-user-02"></use>
+            </svg>
+          </div>
+          <p className={css.text}>Upload photo</p>
+        </div>
+        {/* =============================================================== */}
+        <div className={css.contMyInf}>
+          <h2></h2>
+        </div>
+      </div>
+    </div>
+  );
 };
