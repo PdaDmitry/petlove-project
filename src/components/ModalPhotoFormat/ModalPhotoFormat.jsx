f45b7efa1979567dd9ed99004812a83ffa9ce0ba
PdaDmitry <146446934+PdaDmitry@users.noreply.github.com>
2025-03-06 19:18:07 +0200

    Update ModalPhotoFormat.jsx (added design)

--- src/components/ModalPhotoFormat/ModalPhotoFormat.jsx
+++ src/components/ModalPhotoFormat/ModalPhotoFormat.jsx
@@ -1,28 +1,24 @@
 import { IoMdClose } from 'react-icons/io';
-import { useNavigate } from 'react-router-dom';
 import css from './ModalPhotoFormat.module.css';
 
 export const ModalPhotoFormat = ({ closeModal }) => {
-  const navigate = useNavigate();
-
-  const handleClickBack = () => {
-    navigate('/add-pet');
-  };
-
   return (
     <div className={css.contModalAttention}>
       <button type="button" className={css.closeButton} onClick={closeModal} aria-label="Close">
         <IoMdClose style={{ width: '24px', height: '24px' }} />
       </button>
-
+      <div className={css.backgrImeg}>
+        <img src="/🐶.png" alt="Cat" className={css.imageDog} />
+      </div>
       <h3 className={css.title}>Attention</h3>
       <p className={css.text}>
-        The avatar must be a link to an image in the following formats: png, jpg, jpeg, gif, bmp or
-        webp, starting with http:// or https://.
+        Please enter the URL of the image for the avatar in the “Enter URL” field. The link must
+        begin with http:// or https:// and point to an image in one of the following formats:
+        <br /> PNG, JPG, JPEG, GIF, BMP or WEBP!
       </p>
 
-      <button type="button" className={css.btnLogIn} onClick={handleClickBack}>
-        Log In
+      <button type="button" className={css.btnBack} onClick={closeModal}>
+        Back
       </button>
     </div>
   );
